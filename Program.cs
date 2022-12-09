f3f921bdb0a63f03e27493d3c87825279b4498e9
Wayne Blaylock <40773939+wayneblaylock@users.noreply.github.com>
2022-12-08 19:23:36 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,16 +4,9 @@ using System.Diagnostics;
 ///WAVE file documentation at http://soundfile.sapp.org/doc/WaveFormat/
 
 byte[] fileBytes = File.ReadAllBytes("test_song.wav");
-///var bits = new BitArray(fileBytes);
-
-for(int i = 0; i <= 45; i++){
-Console.WriteLine($"{i}:  {fileBytes[i],2:x}");
-}
-
 
 int bytesValue(byte[] bytes, int offset, int length){
     if (length == 0)return 0;
-
     int total = 0;
     int multiplier = 1;
     for (int i = 0; i < length; i++){
@@ -22,29 +15,53 @@ int bytesValue(byte[] bytes, int offset, int length){
     }
     return total;
 }
-
-Console.WriteLine($"Length of Array: {fileBytes.Length}");
-Console.WriteLine($"Audio Format: {bytesValue(fileBytes, 20, 2)}");
-Console.WriteLine($"Number of Channels: {bytesValue(fileBytes, 22, 2)}");
-Console.WriteLine($"Sample Rate: {bytesValue(fileBytes, 24, 4)}");
-Console.WriteLine($"Bits per Sample: {bytesValue(fileBytes, 34, 2)}");
-
-
-
+//Testing bytesValue Method
 Debug.Assert(bytesValue(new byte[]{3, 7, 8, 2}, 0, 0)==0);
-
 Debug.Assert(bytesValue(new byte[]{3, 7, 8, 2}, 0, 1)==3);
-
 Debug.Assert(bytesValue(new byte[]{3, 7, 8, 2}, 2, 1)==8);
-
 Debug.Assert(bytesValue(new byte[]{36, 8, 0, 0}, 0, 4)==2084);
 
-int dataToHz(int position){
-    
+//Decodes and Saves Necessary Meta-Data
+Console.WriteLine($"Length of Array: {fileBytes.Length}");
+int Array_Length = fileBytes.Length;
+Console.WriteLine($"Audio Format: {bytesValue(fileBytes, 20, 2)}");
+int Audio_Format = bytesValue(fileBytes, 20, 2);
+Console.WriteLine($"Number of Channels: {bytesValue(fileBytes, 22, 2)}");
+int Channels = bytesValue(fileBytes, 22, 2);
+Console.WriteLine($"Sample Rate: {bytesValue(fileBytes, 24, 4)}");
+int Sample_Rate = bytesValue(fileBytes, 24, 4);
+Console.WriteLine($"Bits per Sample: {bytesValue(fileBytes, 34, 2)}");
+int Bits_Per_Sample = bytesValue(fileBytes, 34, 2);
+
+
+//defines a functions that takes the starting position of your bytes, and outputs a usable Hz value
+int dataToHz_16(int position){
+    byte b1 = fileBytes[position];
+    byte b2 = fileBytes[position+1];
+    var raw_num = BitConverter.ToInt16(new byte[] { b1, b2 }, 0);
+    double Hz_value = (0.01* Math.Pow((2), (0.00063876*raw_num)))+37;
+    return (int)Hz_value;
+}
+int dataToHz_8(int position){
+    byte b1 = fileBytes[position];
+
     return 0;
 }
 
+//assemble a list of the samples we want to play, stored as a Hz value
+List<int> Frequencies = new List<int>();
+int Array_Parse_Factor = (Sample_Rate/1000)*Channels;
+if (Bits_Per_Sample == 16) Array_Parse_Factor *= 2;
 
-for (int i = 44; i < fileBytes.Length; i += 350000){
-    Console.WriteLine($"{fileBytes[i],3} {fileBytes[i+1],3}");
-}
+if (Bits_Per_Sample == 16){
+for (int i = 44; i < fileBytes.Length; i += Array_Parse_Factor){
+    Frequencies.Add(dataToHz_16(i));
+    // Console.WriteLine(dataToHz_16(i));
+}}
+else{
+for (int i = 44; i < fileBytes.Length; i += Array_Parse_Factor){
+    Frequencies.Add(dataToHz_8(1));
+}}
+int Frequencies_Length = Frequencies.Count;
+Console.WriteLine("Your Song is playing...");
+for (int i = 0; i <= Frequencies_Length; i++) Console.Beep(Frequencies[i], 500);
